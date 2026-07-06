34c765ea3d3fb2792f297219d91dd70d828bc0b7
NakzzxOfficial <nakularasyidramadhan@gmail.com>
2026-07-06 10:54:37 +0700

    Add event loop setup for asyncio with uvloop
    Handle case where no running event loop is found.

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -1,5 +1,13 @@
+import asyncio
 import uvloop
 
+uvloop.install()
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 uvloop.install()
 
 import logging
